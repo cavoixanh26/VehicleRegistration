3a18e1156d7eb34cd89d19d65da7ae5c1eff686b
ducNT <cavoixanh2112@gmail.com>
2024-02-16 23:20:12 +0700

    add FE verify user

---
+++ VehicleRegistrationPlate/VRP.MVC/Controllers/RegistrationProcedureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VRP.MVC.Controllers
+{
+    [Route("registration")]
+    public class RegistrationProcedureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("car-license-plate")]
+        public IActionResult RegisterLicensePlate()
+        {
+            return View();
+        }
+    }
+}
