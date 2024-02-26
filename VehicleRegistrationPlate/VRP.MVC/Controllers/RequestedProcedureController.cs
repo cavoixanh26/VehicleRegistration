182f36ade13f82ca424d416ac16517396e99180a
ducNT <cavoixanh2112@gmail.com>
2024-02-26 22:27:30 +0700

    comsume getdetail procedure

--- VehicleRegistrationPlate/VRP.MVC/Controllers/RequestedProcedureController.cs
+++ VehicleRegistrationPlate/VRP.MVC/Controllers/RequestedProcedureController.cs
@@ -4,6 +4,7 @@ using VRP.MVC.Repositories.HttpClient;
 
 namespace VRP.MVC.Controllers
 {
+    [Route("requested-procedure")]
     public class RequestedProcedureController : Controller
     {
         private readonly IHttpCallService httpCallService;
@@ -17,6 +18,13 @@ namespace VRP.MVC.Controllers
         {
             var requestedProcedureRequest = new ProcedureRequest();
             var response = await httpCallService.GetData<ProcedureResponse>("RequestedProcedures", requestedProcedureRequest);
+            return View(response);
+        }
+
+        [Route("{id}")]
+        public async Task<IActionResult> GetDetail(int id)
+        {
+            var response = await httpCallService.GetData<UserInformationProcedure>($"RequestedProcedures/{id}", null);
             return View(response);
         }
     }
